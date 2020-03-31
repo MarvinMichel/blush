51b50390612a13f9c5d2152550de63cf7c5a5136
IngeDW <inge_dewaard@hotmail.com>
2020-03-31 18:45:22 +0200

    scss general tags styled

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 8000;
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const mongoose = require('mongoose');
+const passport = require('passport');    // Inge
 require('dotenv').config();
 
 // Connect to database trough Mongoose
@@ -25,8 +26,26 @@ app
   .set('view engine', 'ejs')
   .set('views', 'views')
   .use(express.static(__dirname + '/public'))
+  .use(passport.initialize())                                                   // Inge
+  .use(passport.session())                                                      // Inge
   .use('/', require('./routes/index'))
   .use('/feed', require('./routes/feed'))
+  .get('/login', (req, res) => res.render('index_register_login.ejs'))          // Inge
+  .get('/signup', (req, res) => res.render('index_temp.ejs'))                   // Inge
+  .get( '/add-profile', (req, res) => res.render('feed.ejs'))                   // Inge
+  .post( '/add-profile', (req, res) => res.render('feed.ejs'))                  // Inge
+  .post('/signup', signupToMakeProfile)                                         // Inge
+  .post('/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    (req, res) => {
+      res.redirect('/');
+    })
+
   .listen(port, () => console.log(`Server is running on localhost:${port}`));
+
+// Inge //Function om de data van het signup form in de database op te slaan en te redirecten naar het maken van een profiel
+function signupToMakeProfile(req, res){
+    res.render('index_register_profile.ejs')
+}
 
 exports.urlencodedParser = urlencodedParser;
