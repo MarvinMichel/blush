05053103c383e60ed5d8ee38878eafc31595e538
Martian Code <mmhsernee@gmail.com>
2020-04-09 14:13:57 +0200

    Update chats.js

--- routes/chats.js
+++ routes/chats.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('chats');
+  res.render('chats', { user: req.user });
 });
 
 module.exports = router;
