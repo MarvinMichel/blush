aa41e9286e1b6eae83a0faf1a568ab8527b7ca4d
IngeDW <inge_dewaard@hotmail.com>
2020-04-13 13:55:44 +0200

    Add progress animation

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -34,7 +34,7 @@ clickAway.addEventListener('click', () => {
   event.preventDefault();
   section.classList.remove('apear');
   section.classList.toggle('away');
-  filterButton.classList.remove('away')
+  filterButton.classList.remove('away');
   extraFilters.classList.remove('apear');
   extraFilters.classList.toggle('away');
   basicFilters.classList.remove('apear');
@@ -59,21 +59,40 @@ showButton();
 const matchBtns = document.querySelector('.profile--matching');
 const likeBtn = document.querySelectorAll('[data-like]');
 const dislikeBtn = document.querySelectorAll('[data-dislike]');
+const profiles = document.querySelectorAll('.profile');
+
+// const heart = document.querySelectorAll('#heart');       // Inge
+// const cross = document.querySelectorAll('#cross');       // Inge
+
+for (let el of profiles) {
+  el.addEventListener('click', () => {
+    console.log('Event triggered');
+    el.classList.toggle('click');
+  });
+}
 
 for (let el of likeBtn) {
   el.addEventListener('click', (e) => {
     e.preventDefault();
     const id = el.parentElement.firstElementChild;
+    const likeImg = el.parentElement.parentElement.parentElement.nextSibling.nextSibling;        // Inge
+    console.log(likeImg);
+    likeImg.classList.toggle('heartGone');                            // Inge
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', "/feed", true);
+    xhr.open('POST', '/feed', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(`like=true&id=${id.value}`);
   });
-};
+}
 
 for (let el of dislikeBtn) {
   el.addEventListener('click', (e) => {
     e.preventDefault();
+    e.stopPropagation();
+    const dislikeImg = el.parentElement.parentElement.parentElement.nextSibling.nextSibling.nextSibling;        // Inge
+    console.log(dislikeImg);
+    dislikeImg.classList.toggle('heartGone');
+    // el.classList.toggle('crossGone');                 // Inge
   });
-};
+}
 /* Made by Marvin */
